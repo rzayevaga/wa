bb6510b4a2e74d358edbf375f414652ffd1a164f
Ağa Rzayev <rzayevaga.dev@gmail.com>
2025-03-23 11:26:04 +0400

    Update and rename wa.py to app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template
 
-wa = Flask(__name__)
+app = Flask(__name__)
 
-@wa.route('/')
+@app.route('/')
 def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
     # Bütün interfeyslərdən giriş üçün host='0.0.0.0'
-    wa.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
